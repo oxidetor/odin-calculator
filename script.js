0422f349e3feca68ae266d8141d455afec98d3a6
oxidetor <prateek.girdhar@outlook.com>
2022-06-14 22:18:55 -0700

    Display result of calc on clicking equals btn

--- script.js
+++ script.js
@@ -3,14 +3,6 @@ let subtract = (a, b) => a - b;
 let multiply = (a, b) => a * b;
 let divide = (a, b) => a / b;
 
-function multiply(a, b) {
-  return a * b;
-}
-
-function divide(a, b) {
-  return a / b;
-}
-
 let operation = {
   current_operand: "left",
   left_operand: "",
@@ -19,13 +11,13 @@ let operation = {
   operate() {
     switch (this.operator) {
       case "+":
-        return add(this.left_operand, this.right_operand);
+        return add(+this.left_operand, +this.right_operand);
       case "-":
-        return subtract(this.left_operand, this.right_operand);
+        return subtract(+this.left_operand, +this.right_operand);
       case "*":
-        return multiply(this.left_operand, this.right_operand);
+        return multiply(+this.left_operand, +this.right_operand);
       case "/":
-        return divide(this.left_operand, this.right_operand);
+        return divide(+this.left_operand, +this.right_operand);
       default:
         break;
     }
@@ -38,7 +30,6 @@ digits.forEach((digit) => digit.addEventListener("click", onDigitClick));
 function onDigitClick(e) {
   let digit = e.target.textContent;
   console.log(digit);
-  displayDigit(digit);
 
   if (operation.current_operand == "left") {
     operation.left_operand += digit;
@@ -46,6 +37,7 @@ function onDigitClick(e) {
     operation.right_operand += digit;
   }
 
+  displayOperation();
   console.table(operation);
 }
 
@@ -61,12 +53,33 @@ function onOperatorClick(e) {
   operation.current_operand = "right";
   operation.operator = operator;
 
-  displayDigit(` ${operator} `);
+  displayOperation();
 
   console.table(operation);
 }
 
-function displayDigit(digit) {
+let equals = document.querySelector(".equals");
+equals.addEventListener("click", onEqualsClick);
+
+function onEqualsClick(e) {
+  if (operation.left_operand != "" && operation.right_operand != "") {
+    result = operation.operate();
+    displayResult(result);
+    operation.left_operand = `${result}`;
+    operation.right_operand = "";
+    operation.current_operand = "left";
+  }
+  console.table(operation);
+}
+
+function displayResult() {
   let displayBox = document.querySelector(".display-box");
-  displayBox.textContent += digit;
+  displayBox.textContent = result;
+}
+
+function displayOperation() {
+  let displayBox = document.querySelector(".display-box");
+  displayBox.textContent = `${operation.left_operand}${
+    operation.operator != null ? " " + operation.operator + " " : ""
+  }${operation.right_operand != "" ? " " + operation.right_operand + " " : ""}`;
 }
